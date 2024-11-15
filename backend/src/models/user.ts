0812462bcf88cdbcde6c2be00f8685dbe73d17d7
BlackRose484 <hungnbc2@gmail.com>
2024-11-15 13:06:33 +0700

    [backend]<opt> optimize register function

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,28 +1,27 @@
-
 import mongoose from "mongoose";
 import { UserType } from "./types";
 import bcrypt from "bcryptjs";
 
 // User schema
 const userSchema = new mongoose.Schema<UserType>({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    fullname: { type: String, required: true },
-    sex: { type: String },
-    address: { type: String },
-    phone: { type: String },
-    passport: { type: String },
-    date_of_birth: { type: Date },
-    role: { type: String, enum: ["Customer","Admin"], required: true },
-    nums_booking_changed: { type: Number, default: 0, immutable: true },
-  });
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullname: { type: String },
+  sex: { type: String },
+  address: { type: String },
+  phone: { type: String },
+  passport: { type: String },
+  date_of_birth: { type: Date },
+  role: { type: String, enum: ["Customer", "Admin"] },
+  nums_booking_changed: { type: Number, default: 0, immutable: true },
+});
 
 // Hash the password before saving the user model
 userSchema.pre("save", async function (next) {
-if (this.isModified("password")) {
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 8);
-}
-next();
+  }
+  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
