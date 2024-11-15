2beadfaf27de4db2700a6a4ee5d931a723a758ea
BlackRose484 <hungnbc2@gmail.com>
2024-11-15 13:05:07 +0700

    [backend]<fix> fix bug in making book for flights

--- backend/src/models/flight.ts
+++ backend/src/models/flight.ts
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 import { FlightType } from "./types";
-import { AircraftType } from "./types"; 
+import { AircraftType } from "./types";
 import seatSchema from "./seat";
 import bookingSchema from "./booking";
 
 // Flight schema
 const flightSchema = new mongoose.Schema<FlightType>({
-    aircraft_id: { type: String, required: true },
-    number: { type: String, required: true, unique: true },
-    ori_airport: { type: String, required: true },
-    des_airport: { type: String, required: true },  
-    scheduled_departure: { type: Date, required: true },  
-    scheduled_arrival: { type: Date, required: true },
-    actual_departure: { type: Date },
-    actual_arrival: { type: Date },
-    nums_busi_seat_avail: { type: Number, required: true },
-    nums_eco_seat_avail: { type: Number, required: true },
-    base_price: { type: Number, required: true },
-    revenue: { type: Number, default: 0 ,immutable: true},
-  });
+  aircraft_id: { type: String, required: true },
+  number: { type: String, required: true, unique: true },
+  ori_airport: { type: String, required: true },
+  des_airport: { type: String, required: true },
+  scheduled_departure: { type: Date, required: true },
+  scheduled_arrival: { type: Date, required: true },
+  actual_departure: { type: Date },
+  actual_arrival: { type: Date },
+  nums_busi_seat_avail: { type: Number, required: true },
+  nums_eco_seat_avail: { type: Number, required: true },
+  base_price: { type: Number, required: true },
+  revenue: { type: Number, default: 0 },
+});
 const Flight = mongoose.model<FlightType>("Flight", flightSchema);
 
 export default Flight;
