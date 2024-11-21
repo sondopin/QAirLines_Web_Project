9deda414c6e30797fe1682cc93b51658b5b93104
BlackRose484 <hungnbc2@gmail.com>
2024-11-21 21:35:37 +0700

    [frontend]<refactor> add external guards

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,38 +1,19 @@
-import { useRoutes, Outlet, Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AppContext } from "./context/app.context";
+import { useRoutes } from "react-router-dom";
 import { PATH } from "./constants/path";
 import LoginPage from "./pages/Login/Login";
 import RegisterPage from "./pages/Register/Register";
-import Error from "./pages/Error";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
-
-function ProtectedRoute() {
-  const { isAuthenticated } = useContext(AppContext);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-}
-
-function RejectedRoute() {
-  const { isAuthenticated } = useContext(AppContext);
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-}
-
-function AdminRoute() {
-  const { isAdmin, isAuthenticated } = useContext(AppContext);
-  return isAdmin && isAuthenticated ? (
-    <Outlet />
-  ) : (
-    <Error message="You are not admin" />
-  );
-}
+import PrivateRoute from "./guards/PrivateRoute";
+import AdminRoute from "./guards/AdminRoute";
+import RejectedRoute from "./guards/RejectedRoute";
 
 export default function createRoutes() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router_elements = useRoutes([
     {
       path: "",
-      element: <ProtectedRoute />,
+      element: <PrivateRoute />,
     },
     {
       path: PATH.admin.base,
