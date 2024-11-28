223a98e7332d72c9bf80df0d1b11ade578ad3547
tranminhtuan1709 <minhtuan1231709@gmail.com>
2024-11-28 22:13:23 +0700

    [frontend]<build> Create AdjustedFlightNotification component

--- frontend/src/pages/MyBooking.tsx
+++ frontend/src/pages/MyBooking.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Booking from "../components/Booking";
+import AdjustedFlightNotification from "../components/AdjustedFlightNotification";
 
 interface MyBookingProps {
   something?: string;
@@ -43,6 +44,15 @@ const MyBooking: React.FC<MyBookingProps> = ({ something }) => {
           totalPrice={361500000}
           cancelAvailableUntil="Monday, 21/11/2024"
         />
+        <div className="px-[150px]">
+          <AdjustedFlightNotification
+            oldDepartureDate="Tue, 22/11/2024, 10:30 AM"
+            newDepartureDate="Wed, 23/11/2024, 09:00 AM"
+            oldReturnDate="Sat, 25/12/2024, 08:15 PM"
+            newReturnDate="Sun, 26/12/2024, 04:30 PM"
+            reason="Bad weather conditions"
+          />
+        </div>
         <Booking
           bookingDate="19/11/2024"
           status="Up Coming"
