00426b0f62486bde589504e6e99625ada3afb1f6
BlackRose484 <hungnbc2@gmail.com>
2024-12-06 19:55:18 +0700

    [frontend]<opt> optimize UI in view blog for user

--- frontend/src/components/LatestNews.tsx
+++ frontend/src/components/LatestNews.tsx
@@ -1,32 +1,40 @@
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import LatestNewsCard from "./LatestNewsCard";
 import { BlogCard } from "../types/blogs.type";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getLatestBlogs } from "../apis/blogs.api";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { SRC } from "../constants/src";
 
 const LatestNews = () => {
+  const sliderRef = useRef<Slider>(null); // Tham chiếu đến slider
+
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1,
-          infinite: false,
+          infinite: true,
           dots: true,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
           dots: true,
         },
       },
@@ -35,20 +43,20 @@ const LatestNews = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: false,
+          infinite: true,
           dots: true,
         },
       },
     ],
   };
 
-  const { data: blogs_data } = useQuery({
+  const { data: blogs_data = { data: [] } } = useQuery({
     queryKey: ["blogs"],
-    queryFn: () => {
-      return getLatestBlogs();
-    },
-    placeholderData: keepPreviousData,
+    queryFn: getLatestBlogs,
   });
+
+  const handleNext = () => sliderRef.current?.slickNext(); // Xử lý khi nhấn nút Next
+  const handlePrev = () => sliderRef.current?.slickPrev(); // Xử lý khi nhấn nút Previous
 
   return (
     <div className="bg-white justify-center w-2/3">
@@ -59,16 +67,32 @@ const LatestNews = () => {
           </h1>
         </div>
 
-        <Slider {...settings} className="gap-5">
-          {blogs_data?.data.map((blog: BlogCard, index: number) => (
-            <LatestNewsCard
-              key={index}
-              title={blog.title}
-              description={blog.description}
-              image_url={blog.image_url}
-            />
-          ))}
-        </Slider>
+        <div className="relative">
+          <button
+            onClick={handlePrev}
+            className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full z-10"
+          >
+            Prev
+          </button>
+          <button
+            onClick={handleNext}
+            className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full z-10"
+          >
+            Next
+          </button>
+
+          <Slider ref={sliderRef} {...settings} className="gap-5">
+            {blogs_data.data.map((blog: BlogCard, index: number) => (
+              <LatestNewsCard
+                _id={blog._id}
+                key={index}
+                title={blog.title}
+                subtitle={blog.subtitle}
+                cover_url={`${SRC.blog_cover}${blog.cover_url}`}
+              />
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
